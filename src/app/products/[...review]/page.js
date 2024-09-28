6359980192275031843dc8c036ac4ac85ca161af
Anirudha <beleanirudha8@gmail.com>
2024-09-28 16:46:51 +0530

    understanding the concept of 'catch-all-routes'

---
+++ src/app/products/[...review]/page.js
@@ -0,0 +1,13 @@
+export default function ProductReview() {
+	return (
+		<div>
+			<h1>
+				This is the Product Review page, which are reviewed by different
+				users
+				<br />
+				And this will catch all the routes / segments i.e multiple slugs
+				in the route
+			</h1>
+		</div>
+	);
+}
