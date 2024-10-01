3174ad506005b6e66e3790c0e70462fb6058bfcf
Anirudha <beleanirudha8@gmail.com>
2024-10-01 20:58:17 +0530

    adding routes path in account page using "useRouter"

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,9 +1,27 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const Account = () => {
+	const router = useRouter();
+
 	return (
 		<div>
 			<h1>Account page</h1>
+			<div className="mt-2 flex gap-2">
+				<button
+					className="border-[2px] border-black p-2 rounded-lg"
+					onClick={() => router.push("account/dashboard")}
+				>
+					Goto Account Dashboard
+				</button>
+				<button
+					className="border-[2px] border-black p-2 rounded-lg"
+					onClick={() => router.push("account/membership")}
+				>
+					Goto Membership Sectoin
+				</button>
+			</div>
 		</div>
 	);
 };
